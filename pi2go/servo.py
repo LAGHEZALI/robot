f35adb80c0e1030e2a5ec67530b0cd0e09521283
LAGHEZALI <laghezali.mohamed.reda@gmail.com>
2018-02-18 11:51:48 +0000

    servo script v4

--- pi2go/servo.py
+++ pi2go/servo.py
@@ -35,28 +35,29 @@ def readkey(getchar_fn=None):
 pi2go.init()
 
 position = 50 # between 0 and 100
+step = 2
 
 try:
 	while True:
 		keyp = readkey()
 		if keyp == 'i':
-			pi2go.servoMiddle()
+			pi2go.setServo(50)
 			print 'Servo set to Middle Position'
 		elif keyp == 'o':
-			pi2go.servoLeft()
+			pi2go.setServo(0)
 			print 'Servo set to Left Position'
 		elif keyp == 'p':
-			pi2go.servoRight()
+			pi2go.setServo(100)
 			print 'Servo set to Right Position'
 		
 		elif keyp == 'y':
-			position +=10
+			position +=step
 			pi2go.setServo(position)
-			print 'Servo Position ++ = ', position
+			print 'Servo Position +', step, '=', position
 		elif keyp == 'u':
-			position -=10
+			position -=step
 			pi2go.setServo(position)
-			print 'Servo Position -- = ', position
+			print 'Servo Position -', step, '=', position
 		elif keyp == ' ':
 			pi2go.stopServo()
 			print 'Stop Servo'
